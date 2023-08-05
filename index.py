fcf4218ffdcc165105e7013a9586671235e9ac00
Martin Betz <martinmichaelbetz@gmail.com>
2023-08-05 14:48:24 +0200

    Update: 2023-08-05

--- index.py
+++ index.py
@@ -1,5 +1,7 @@
 import things
 all_todos = things.todos()
+work_projects = things.projects(area = '3VFHRj4kQW1TL2mrJ1sWYN')
 
 filtered_todos = [todo for todo in all_todos if not (todo['start'] == 'Someday' and todo['start_date'] is None)]
 print(f"You have {len(filtered_todos)} incomplete tasks in total")
+print(f"{work_projects}")
